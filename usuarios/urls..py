cd9a37f959993e801d3195e48a50e1ed3ad4bb30
Kaique <kaique_candido@hotmail.com>
2024-04-18 10:21:39 -0300

    criando cadastro de usuarios e login

--- usuarios/urls..py
+++ usuarios/urls..py
@@ -1,6 +1,8 @@
 from django.urls import path
+from usuarios import login , cadastro
 
 
 urlpatterns = [
-   
+   path('login',login, name='login'),
+   path('cadastro',cadastro, name='cadastro'),
 ]
